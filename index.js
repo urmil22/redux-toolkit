56b7add4a750f79f4ccae49ad84f34d25411bc00
urmil22 <urmilbhavsar8@gmail.com>
2022-07-14 12:31:42 +0530

    bind action creators

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const redux = require('redux')
 const createStore = redux.createStore
+const bindActionCreators = redux.bindActionCreators
 
 const CAKE_ORDERED = 'Cake_Ordered'
+const CAKE_RESTOCKED = 'CAKE_RESTOCKED'
 
 function orderCake() {
 return {
     type: CAKE_ORDERED,
-    quantity: 1
+    payload: 1
    }
+}
+
+function restockCake(qty=1) {
+    return {
+        type: CAKE_RESTOCKED,
+        payload: qty,
+    }
 }
 
 const initialState = {
@@ -22,6 +31,11 @@ const reducer = (state = initialState, action) => {
                 ...state,
                 numOfCakes: state.numOfCakes - 1
             }
+        case CAKE_RESTOCKED:
+            return {
+                ...state,
+                numOfCakes: state.numOfCakes + action.payload,
+            }
             default:
                 return state
     }
@@ -32,8 +46,10 @@ console.log('initial state', store.getState())
 
 const unsubscribe = store.subscribe(() => console.log("update state ", store.getState()))
 
-store.dispatch(orderCake())
-store.dispatch(orderCake())
-store.dispatch(orderCake())
+const actions = bindActionCreators({ orderCake, restockCake }, store.dispatch)
 
+actions.orderCake()
+actions.orderCake()
+actions.orderCake()
+actions.restockCake(3)
 unsubscribe()
